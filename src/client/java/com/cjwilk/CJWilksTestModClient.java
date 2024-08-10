a8549d182b53cae5e797678b9ad82805f6311f40
Jacob Wilkus <jwilkus1@gmail.com>
2024-08-09 18:27:57 -0600

    initial mod files

---
+++ src/client/java/com/cjwilk/CJWilksTestModClient.java
@@ -0,0 +1,10 @@
+package com.cjwilk;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CJWilksTestModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
